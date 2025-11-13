7c4d924412aabae1c330c9a8ecf50ed2b34aaa5b
jlmunozpb <jlmunozpb@gmail.com>
2025-11-13 10:20:51 -0500

    api

--- src/services/api/api.js
+++ src/services/api/api.js
@@ -5,11 +5,8 @@ const dbHost = process.env.EXPO_PUBLIC_BACKEND_HOST ?? '';
 const dbPort = process.env.EXPO_PUBLIC_BACKEND_PORT ?? '';
 const dbBaseURL = `http://${dbHost}:${dbPort}`;
 
-// Para producción (PROD)
-const dbBaseURLProd = process.env.EXPO_PUBLIC_BACKEND_URL ?? null;
-
 // URL base según el entorno PROD o DEV
-const baseURL = dbBaseURLProd || dbBaseURL;
+const baseURL = process.env.EXPO_PUBLIC_BACKEND_URL_PROD || dbBaseURL;
 
 export const api = axios.create({
   baseURL,
